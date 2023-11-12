a5a3b4416d6d8745090410d3e0fad7a240b6e964
FerminT04 <fermint0401@gmail.com>
2023-11-11 20:35:42 -0600

    Cambio

---
+++ Clases/Conexion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Clave1_Grupo1.Clases
+{
+    class Conexion
+    {
+        //static void Main(string[] args)
+        //{
+        //    string servidor = "localhost";
+        //    string baseDatos = "clave1_grupo1db";
+        //    string usuario = "root";
+        //    string contraseña = "root";
+
+        //    string cadenaConexion = $"Server={servidor};Database={baseDatos};User ID={usuario};Password={contraseña};";
+
+        //    try
+        //    {
+        //        using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+        //        {
+        //            conexion.Open();
+
+        //            Console.WriteLine("Conexión exitosa a la base de datos.");
+
+
+        //            conexion.Close();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"Error al conectar a la base de datos: {ex.Message}");
+        //    }
+
+        //    Console.ReadKey();
+        //}
+    }
+}
